736bf2cd3e8ec58e6517a62af010fbe7453159a6
Matthew Lyons <mattewjosephlyons@gmail.com>
2023-07-06 11:42:28 -0500

    Update prisma schema and create Zod schema/validation

---
+++ server/src/schemas/user.schema.ts
@@ -0,0 +1,47 @@
+import { object, string, TypeOf, z } from 'zod';
+
+enum RoleEnumType {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+
+export const createUserSchema = object({
+  body: object({
+    name: string({
+      required_error: 'Name is required',
+    }),
+    email: string({
+      required_error: 'Email is required',
+    }).email('Invalid email address'),
+    password: string({
+      required_error: 'Password is required',
+    })
+      .min(8, 'Password must be at least 8 characters long')
+      .max(100, 'Password must be at most 100 characters long'),
+    passwordConfirm: string({
+      required_error: 'Please confirm your password',
+    }),
+    role: z.optional(z.nativeEnum(RoleEnumType)),
+  }).refine((data) => data.password === data.passwordConfirm, {
+    path: ['passwordConfirm'],
+    message: 'Passwords do not match',
+  }),
+});
+
+export const loginUserSchema = object({
+  body: object({
+    email: string({
+      required_error: 'Email is required',
+    }).email('Invalid email address'),
+    password: string({
+      required_error: 'Password is required',
+    }).min(8, 'Password must be at least 8 characters long'),
+  }),
+});
+
+export type CreateUserInput = Omit<
+  TypeOf<typeof createUserSchema>['body'],
+  'passwordConfirm'
+>;
+
+export type LoginUserInput = TypeOf<typeof loginUserSchema>['body'];
