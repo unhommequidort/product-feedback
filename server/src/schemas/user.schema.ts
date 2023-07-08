095af06b4a00008ee030b6b626c69f40bd2eceaa
Matthew Lyons <mattewjosephlyons@gmail.com>
2023-07-08 07:29:05 -0500

    Forgot/reset password and updateUser schema changes

--- server/src/schemas/user.schema.ts
+++ server/src/schemas/user.schema.ts
@@ -42,16 +42,61 @@ export const loginUserSchema = object({
   }),
 });
 
-export type RegisterUserInput = Omit<
-  TypeOf<typeof registerUserSchema>['body'],
-  'passwordConfirm'
->;
-
 export const verifyEmailSchema = object({
   params: object({
     verificationCode: string(),
   }),
 });
 
+export const updateUserSchema = object({
+  body: object({
+    name: string({}),
+    email: string({}).email('Invalid email address'),
+    password: string({})
+      .min(8, 'Password must be more than 8 characters')
+      .max(32, 'Password must be less than 32 characters'),
+    passwordConfirm: string({}),
+    role: z.optional(z.nativeEnum(RoleEnumType)),
+  })
+    .partial()
+    .refine((data) => data.password === data.passwordConfirm, {
+      path: ['passwordConfirm'],
+      message: 'Passwords do not match',
+    }),
+});
+
+export const forgotPasswordSchema = object({
+  body: object({
+    email: string({
+      required_error: 'Email is required',
+    }).email('Email is invalid'),
+  }),
+});
+
+export const resetPasswordSchema = object({
+  params: object({
+    resetToken: string(),
+  }),
+  body: object({
+    password: string({
+      required_error: 'Password is required',
+    }).min(8, 'Password must be at least 8 characters'),
+    passwordConfirm: string({
+      required_error: 'Please confirm your password',
+    }),
+  }).refine((data) => data.password === data.passwordConfirm, {
+    message: 'Passwords do not match',
+    path: ['passwordConfirm'],
+  }),
+});
+
+export type RegisterUserInput = Omit<
+  TypeOf<typeof registerUserSchema>['body'],
+  'passwordConfirm'
+>;
+
 export type VerifyEmailInput = TypeOf<typeof verifyEmailSchema>['params'];
 export type LoginUserInput = TypeOf<typeof loginUserSchema>['body'];
+export type UpdateUserInput = TypeOf<typeof updateUserSchema>['body'];
+export type ForgotPasswordInput = TypeOf<typeof forgotPasswordSchema>['body'];
+export type ResetPasswordInput = TypeOf<typeof resetPasswordSchema>;
