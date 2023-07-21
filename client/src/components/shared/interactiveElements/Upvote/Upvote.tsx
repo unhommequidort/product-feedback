64fc39f7b98e49194b73aafde70149e7cbe0a25c
Matthew Lyons <mattewjosephlyons@gmail.com>
2023-07-21 10:46:38 -0500

    Add aria-label to button and empty alt on SVG

--- client/src/components/shared/interactiveElements/Upvote/Upvote.tsx
+++ client/src/components/shared/interactiveElements/Upvote/Upvote.tsx
@@ -8,6 +8,7 @@ interface UpvoteProps {
 export const Upvote = ({ value = 0, onClick }: UpvoteProps) => {
   return (
     <button
+      aria-label="Upvote"
       className="flex flex-col justify-center items-center w-10 h-[3.3125rem] bg-violet-50 rounded-[0.625rem] text-center text-slate-600 text-[0.8125rem] font-bold gap-[0.2rem] hover:bg-indigo-200 transition-all duration-100 ease-in-out border-none active:bg-indigo-600 active:text-white"
       onClick={onClick}
     >
